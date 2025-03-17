72033ab5dc659f003f82524bf7aec35bc7c63c37
Henok Birhanu <hbirhanu.dev@gmail.com>
2025-03-17 14:54:34 +0000

    fix

--- src/lib/pusher/pusher.ts
+++ src/lib/pusher/pusher.ts
@@ -2,17 +2,17 @@ import Pusher from "pusher-js";
 import PusherServer from "pusher";
 
 export const pusher = new PusherServer({
-  appId:'1952166' ,
-  key:'da3fc34f0f63d9c96112' ,
-  secret: '6462241abed3071e8423',
-  cluster: 'ap2',
+  appId: ,
+  key: ,
+  secret: ,
+  cluster:,
   useTLS: true
 });
 
 
 
-export const pusherClient = new Pusher('da3fc34f0f63d9c96112', {
-  cluster: 'ap2',
+export const pusherClient = new Pusher(, {
+  cluster: ,
   forceTLS: true,
 });
 
